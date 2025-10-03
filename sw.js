a2b7aa43b483322c1396cbf175ae1ba30afda523
georgbo94 <62307584+georgbo94@users.noreply.github.com>
2025-10-03 11:35:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/Pitch-Set-Trainer';
 var APP_PREFIX = 'pst_';
-var VERSION = 'version_002';
+var VERSION = 'version_003';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
